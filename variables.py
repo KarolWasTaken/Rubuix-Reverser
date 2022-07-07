536926be3483b5d9caad556c01cb7aca108d4d94
Tohru <95445436+TohruTheMaid@users.noreply.github.com>
2022-07-07 01:06:47 +0100

    variables both files need to access

---
+++ variables.py
@@ -0,0 +1,15 @@
+import sys, os
+
+
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try: 
+        # PyInstaller creates a temp folder and stores path in _MEIPASS             # thanks stackoverflow :)
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+    return os.path.join(base_path, relative_path)
+
+ontop = False
+moves_done = []

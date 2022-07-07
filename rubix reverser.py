d512909ef13bf8f269800de4004cde511abc7f2c
Tohru <95445436+TohruTheMaid@users.noreply.github.com>
2022-07-07 01:05:39 +0100

    Update rubix reverser.py

--- rubix reverser.py
+++ rubix reverser.py
@@ -1,20 +1,12 @@
 import tkinter as tk
+import buttons as bt
 from tkinter import *
 from tkinter import ttk
-import sys, os
+import variables
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try: 
-        # PyInstaller creates a temp folder and stores path in _MEIPASS             # thanks stackoverflow :)
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    return os.path.join(base_path, relative_path)
 
-global moves_done
-moves_done = []
+variables.moves_done = []
 moves_available = [
     "U",
     "D",
@@ -38,37 +30,35 @@ moves_available = [
 
 root = Tk()
 root.title("Rubix Reverser")
-root.iconbitmap(resource_path("rubix.ico"))
+root.iconbitmap(variables.resource_path("rubix.ico"))
 canvas = tk.Canvas(root, width = 500, height=240)
 canvas.pack()
 
+
 labelTitle = tk.Label(root, text= "Welcome to the Rubix Reverser", font=("Yu Gothic", 25))    # Courier Century Gothic
-labelSubtitle = tk.Label(root, text= "Enter your moves", font=("Franklin Gothic Book", 15, "bold"))
+labelSubtitle = tk.Label(root, text= "Enter your moves (B for buttons)", font=("Franklin Gothic Book", 15, "bold"))
 canvas.create_rectangle(450, 120.5, 60, 118.5, fill='black')
 
 def comboadd(event):
-    global moves_done
 
-    moves_done.append(myCombo.get())
+    variables.moves_done.append(myCombo.get())
     label = Label(root, text ="                        added                          ")
     canvas.create_window(250, 220, window=label)
 
 def reverse():
-    global moves_done
-    if len(moves_done) > 0:
-        moves_done.reverse()
-        moves = ' '.join(moves_done)
+    if len(variables.moves_done) > 0:
+        variables.moves_done.reverse()
+        moves = ' '.join(variables.moves_done)
          
         label = Label(root, text ="               Reversed: " + str(moves) + "                ")
         canvas.create_window(250, 220, window=label)                                  
-        moves_done = []
+        variables.moves_done = []
     else:  
         label = Label(root, text ="                           No moves to reverse                           ")
         canvas.create_window(250, 220, window=label)                                  
 
 def remove_move():
-    global moves_done
-    moves_done.pop() 
+    variables.moves_done.pop() 
     label = Label(root, text ="                     Removed last move                     ")
     canvas.create_window(250, 220, window=label)
 
@@ -83,5 +73,9 @@ canvas.create_window(290, 190, window= myButtonRemove)
 
 canvas.create_window(250, 50, window=labelTitle)
 canvas.create_window(250, 100, window=labelSubtitle)
+
+
+root.bind("<b>", bt.buttonBox)
+root.bind("<B>", bt.buttonBox)
 root.resizable(False, False) # stop window from being resized
 root.mainloop()  
